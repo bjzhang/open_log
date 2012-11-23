00e4fba6b44428ec997412cd51dfa1c1f2d156b7
bjzhang@suse.com <bjzhang@suse.com>
2012-11-23 17:23:47 +0800

    add test script for managedsave, NOT FINISHED

---
+++ novell/hypervisor_and_tools/libvirt/managedsave/loop_test.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+
+test_ret()
+{
+    if [ $1 -ne 0 ]; then
+        echo "###Error: fail, exit"
+        exit 1
+    else
+        echo "###successful"
+    fi
+}
+
+wait_started()
+{
+    domain_id=`virsh list | grep $domain_name | cut -d \  -f 2`
+    echo domain id is $domain_id 
+    echo "waiting domain started"
+    last_cpu_time=`virsh dominfo $domain_name | grep "CPU time" | cut -d : -f 2 | sed s/^\ *//`
+    sleep 1
+    echo -n "cpu time is "
+    while true; do
+        cpu_time=`virsh dominfo $domain_name | grep "CPU time" | cut -d : -f 2 | sed s/^\ *//`
+        echo -n "$cpu_time, "
+        if [ "$cpu_time" == "$last_cpu_time" ]; then
+            echo cpu time is $cpu_time. $domain_name fully started. 
+            break 
+        fi
+        last_cpu_time=$cpu_time
+        sleep 2
+    done
+}
+
+reboot_or_shutdown()
+{
+    action=$1
+
+    domain_id=`virsh list | grep $domain_name | cut -d \  -f 2`
+    wait_started
+    echo "$action $domain_name"
+    virsh $action $domain_name
+    echo -n "wait for $domain_name $domain_id ${action}ing"
+    while true; do 
+        virsh list | grep $domain_name | grep $domain_id 2>&1 > /dev/null || break
+        echo -n "."
+        sleep 2
+    done
+    ret=$?
+    echo 
+    echo "$domain_name $domain_id ${action}ed"
+    if [ $action == "reboot" ]; then
+        virsh list | grep $domain_name 
+        test_ret $ret
+    elif [ $action == "shutdown" ]; then
+        test_ret $ret
+    fi
+}
+
+if [ -z $1 ]; then
+    echo "Error: demain xml missing, exit". 
+    exit 1
+fi
+
+domain_xml=$1
+domain_name=`cat $domain_xml | grep "<name>" | sed "s/<name>\(.*\)<\/name>/\1/" | sed "s/^\ *//"`
+echo "domain_xml is $domain_xml; domain_name is $domain_name"
+
+echo "define domain as $domain_name from $domain_xml"
+virsh define $domain_xml
+echo -n "define result..."
+virsh list --all --state-shutoff | grep $domain_name
+test_ret $?
+echo -n "start vm..."
+virsh start $domain_name
+wait_started
+echo -n "managed save..."
+virsh managedsave $domain_name
+test_ret $?
+
+
+#echo "restarting libvirtd before test"
+##rclibvirtd restart
+#for i in `seq 10000`; do
+#    echo "test $i times"
+#    echo "test libxlDomainCreateXML: create"
+#    virsh create $domain_xml
+#    virsh list | grep $domain_name -w
+#    test_ret $?
+#    wait_started
+#
+#    echo "sleep 20 second"
+#    for t in `seq 20`; do
+#        date
+#        virsh list
+#        test_ret $?
+#        ping -c 4 192.168.122.59
+#        test_ret $?
+#        sleep 1
+#    done
+#
+#    echo "test libxlDomainReboot: reboot"
+#    reboot_or_shutdown reboot
+#
+#    echo "test libxlShutdownFlags: shutdown"
+#    reboot_or_shutdown shutdown
+#    sleep 1
+#
+#    echo "sleep 10 second"
+#    for t in `seq 20`; do
+#        date
+#        virsh list
+#        test_ret $?
+#        sleep 1
+#    done
+#
+#done
+
