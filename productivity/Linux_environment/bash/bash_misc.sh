e4e163bc04e06bbafc3e0bcae5b2238baa1304db
Bamvor Jian Zhang <bamvor.zhangjian@linaro.org>
2016-12-05 19:07:02 +0800

    open log: 20161205
    Signed-off-by: Bamvor Jian Zhang <bamvor.zhangjian@linaro.org>

--- productivity/Linux_environment/bash/bash_misc.sh
+++ productivity/Linux_environment/bash/bash_misc.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-. /home/z00293696/works/reference/open_log/productivity/Linux_environment/bash/gdb_debug
-. /home/z00293696/works/reference/open_log/productivity/Linux_environment/bash/logAssistant_helper
-. /home/z00293696/works/reference/open_log/productivity/Linux_environment/bash/indent-libvirt
-. /home/z00293696/works/source/linux_toolkit/bash_function/bash_function
+source /home/z00293696/works/reference/open_log/productivity/Linux_environment/bash/gdb_debug
+source /home/z00293696/works/reference/open_log/productivity/Linux_environment/bash/logAssistant_helper
+source /home/z00293696/works/reference/open_log/productivity/Linux_environment/bash/indent-libvirt
+source /home/z00293696/works/source/linux_toolkit/bash_function/bash_function
