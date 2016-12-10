94640a5439a4ac9b3d1939ace368fec92947f700
Bamvor Jian Zhang <bamvor.zhangjian@linaro.org>
2016-12-10 11:34:54 +0800

    add sourced file to bash.bashrc instead of /etc/profile.d
    Signed-off-by: Bamvor Jian Zhang <bamvor.zhangjian@linaro.org>

--- productivity/Linux_environment/bash/bash_misc.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-source /home/z00293696/works/reference/open_log/productivity/Linux_environment/bash/gdb_debug
-source /home/z00293696/works/reference/open_log/productivity/Linux_environment/bash/logAssistant_helper
-source /home/z00293696/works/reference/open_log/productivity/Linux_environment/bash/indent-libvirt
-source /home/z00293696/works/source/linux_toolkit/bash_function/bash_function
