c3a3f24ceae9567a0c1af51b2e1f813bee70de97
Bamvor Zhang <bamv2005@gmail.com>
2018-01-10 22:24:10 +0800

    tidb: the first version of TiDB environment setup
    Signed-off-by: Bamvor Zhang <bamv2005@gmail.com>

--- eking/TiDB/bootstrap.sh
+++ eking/TiDB/bootstrap.sh
@@ -3,23 +3,19 @@
 die()
 {
 	echo $1
+	exit
 }
 
 deploy_user=$1
 deploy_dir=$2
-target=$3
-dryrun="true"
+#dryrun="true"
 
 if [ "$deploy_user" = "" ]; then
-    deploy_user=etidb
+    deploy_user=tidb
 fi
 
 if [ "$deploy_dir" = "" ]; then
     deploy_dir=/home/$deploy_user/deploy
-fi
-
-if [ "$target" = "" ]; then
-    die "Target could not empty. exit"
 fi
 
 if [ "$dryrun" = "true" ]; then
@@ -28,15 +24,34 @@ else
         LIMITS_CONF="/etc/security/limits.conf"
 fi
 
+echo "Set language to English UTF8"
+LANG = en_US.UTF8
+
 echo "Checking whether disk is full or not"
-ssh $target "cd `dirname $deploy_dir`; df -h . |tail -n1 | grep 100%"
+cd `dirname $deploy_dir`; df -h . |tail -n1 | grep 100%
 if [ "$?" = "0" ]; then
         die "Disk is full in $deploy_dir. Exit"
 fi
 
+#echo "Installing python"
+##centos
+#yum install python
+##suse
+##zypper install python
+
+echo "Install ntp"
+#centos
+echo y | yum install ntp
+#euse
+#zypper --non-interactive install ntp
+
 echo "Checking date(ntp)"
-#check ntp
-echo "TODO ntp, exit"
+systemctl enable ntpd
+systemctl start  ntpd
+ntpstat | grep -w synchronised | wc -l
+#HT=`date "+%s"`
+#TARGET=root@10.1.1.100; TT=`ssh $TARGET "date '+%s'"`
+#echo $((HT - TT))
 
 echo "Configuration system and user limitation"
 sysctl -w net.core.somaxconn=32768
@@ -67,15 +82,20 @@ mkdir -p $deploy_dir
 chown -R $deploy_user:$deploy_user $deploy_dir
 chmod 755 $deploy_dir
 
-echo "TODO"
-
 echo "Disable Firewall"
 #Centos
+systemctl disable firewalld
+systemctl stop firewalld
 #Opensuse
-SuSEfirewall2 off
+#SuSEfirewall2 off
 
 #echo "Enable Firewall"
 ##Centos
+#systemctl enable firewalld
+#systemctl start firewalld
 ##Opensuse
 #SuSEfirewall2 on
+
+#echo "Configuration irqbalance"
+#enable IRQBALANCE_ONESHOT to run irqbalance only once.
 
