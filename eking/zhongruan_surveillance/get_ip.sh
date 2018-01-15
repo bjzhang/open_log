3d679097813f330d77f8010088d2391f73807563
Bamvor Jian Zhang <bamv2005@gmail.com>
2018-01-15 22:29:15 +0800

    vm helper: use VIRSH in order to maintainer the code easier
    Signed-off-by: Bamvor Jian Zhang <bamv2005@gmail.com>

--- eking/zhongruan_surveillance/get_ip.sh
+++ eking/zhongruan_surveillance/get_ip.sh
@@ -2,16 +2,17 @@
 #Try to get ip through arp with 10 seconds timeout.
 #Arguments: $1 name of vm: could be name, uuid, ID any of which could be
 #                          supported by virsh
+VIRSH="sudo virsh"
 get_ip() {
     NAME=$1
-    virsh list --all | grep $NAME -w 2>&1 > /dev/null
+    $VIRSH list --all | grep $NAME -w 2>&1 > /dev/null
     if [ "$?" != "0" ]; then
         echo "Error: vm($NAME) inexist. exit"
         return
     fi
 
-    bridge=`virsh dumpxml $NAME | grep source.bridge | cut -d \' -f 2`
-    mac=`virsh dumpxml $NAME |grep mac.address | cut -d = -f 2 | sed "s/['/>']//g"`
+    bridge=`$VIRSH dumpxml $NAME | grep source.bridge | cut -d \' -f 2`
+    mac=`$VIRSH dumpxml $NAME |grep mac.address | cut -d = -f 2 | sed "s/['/>']//g"`
     for i in `seq 10`; do
         ip=`arp -a -i $bridge | grep $mac | cut -d \  -f 2 | sed "s/[()]//g"`
         if [ "$ip" != "" ]; then
