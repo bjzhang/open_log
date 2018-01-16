e073dbaab888acfa161afeeac5709456fca95295
Bamvor Jian Zhang <bamv2005@gmail.com>
2018-01-16 16:26:09 +0800

    vm helper: use absolute path for device xml
    Signed-off-by: Bamvor Jian Zhang <bamv2005@gmail.com>

--- eking/zhongruan_surveillance/insert_data_disk.sh
+++ eking/zhongruan_surveillance/insert_data_disk.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 #NAME=ceph_test_0.5.4_02
+DIR=`which $0`
+DIR=`dirname $DIR`
 NAME=$1
 if [ "$1" = "" ]; then
 	exit
 fi
 VDISK=$2
+#VDISK is used for host. we could not determine the device name in target(AKA: guest)
 if [ "$VDISK" = "" ]; then
 	exit
 fi
 DISK=`echo ${NAME} | sed 's/test/data/g'`_${VDISK}.raw
-XML=ceph-data.xml
+XML=${DIR}/ceph-data.xml
 
 if [ -f "/mnt/images/$DISK" ]; then
 	echo "$DISK exist. exit"
