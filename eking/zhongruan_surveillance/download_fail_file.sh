729232a01ea86e0c6ea8a46d08df857921c981c8
Bamvor Jian Zhang <bamv2005@gmail.com>
2018-01-18 21:58:00 +0800

    WIP: kiwi helper: download file which failed in previous kiwi
    Signed-off-by: Bamvor Jian Zhang <bamv2005@gmail.com>

---
+++ eking/zhongruan_surveillance/download_fail_file.sh
@@ -0,0 +1,24 @@
+
+for url in `grep Timeout /home/vagrant/works/software/kiwi/build/image-root.log  | sed "s/^.*Timeout.on \(.*.\): (.*$/\1/g" | sort | uniq`; do
+	echo url is $url
+	repo=${url%/*}
+	repo=`echo $repo | sed "s/repodata//g" | sed "s/%3A/:/g"`
+	#    <repository type="rpm-md" alias="KIWI-NG_Appliance">
+	alias=`grep $repo /home/vagrant/works/source/kiwi-descriptions/centos/x86_64/centos-07.0-JeOS/config.xml -B 1 | head -n1`
+	alias=`echo $alias | cut -d = -f 3 | sed "s/[\">]//g"`
+	echo "repo alias is $alias"
+	REPO_PATH=/var/cache/kiwi/yum/cache/$alias
+	name=${url##*//}
+	name=${name##*/}
+	echo package name is $name
+	echo $name | grep "\.rpm$"
+	if [ "$?" = "0" ]; then
+		echo "rpm package. download to $REPO_PATH/packages"
+		PACKAGE_FULL_NAME=$REPO_PATH/packages/$name
+		sudo rm $PACKAGE_FULL_NAME
+		sudo wget --directory-prefix `dirname $PACKAGE_FULL_NAME` $url
+	else
+		sudo rm /var/cache/kiwi/yum/cache/$alias/$name
+		sudo wget --directory-prefix $REPO_PATH $url
+	fi
+done
