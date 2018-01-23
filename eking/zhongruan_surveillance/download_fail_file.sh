c32e54ca4ab4ccf9d428183f2e11ba8a108be86e
Bamvor Jian Zhang <bamv2005@gmail.com>
2018-01-23 14:40:48 +0800

    kiwi helper: download files: get repo type at the beginning; get the correct alias name with or without priority=NUM attribute
    Signed-off-by: Bamvor Jian Zhang <bamv2005@gmail.com>

--- eking/zhongruan_surveillance/download_fail_file.sh
+++ eking/zhongruan_surveillance/download_fail_file.sh
@@ -5,11 +5,7 @@ download_url()
 {
 	url=$1
 	#echo url is $url
-	repo=${url%/*}
-	repo=`echo $repo | sed "s/repodata//g" | sed "s/%3A/:/g"`
-	repo=${repo##*//}
-	repo=${repo#*/}
-	echo $repo | grep opensuse -i
+	echo $url | grep opensuse -i
 	if [ "$?" = "0" ]; then
 		repo_type=opensuse
 	else
@@ -22,6 +18,10 @@ download_url()
 			exit
 		fi
 	fi
+	repo=${url%/*}
+	repo=`echo $repo | sed "s/repodata//g" | sed "s/%3A/:/g"`
+	repo=${repo##*//}
+	repo=${repo#*/}
 	if [ "${repo_type}" = "opensuse" ]; then
 		repo=`echo $repo | sed "s/repositories\///g"`
 		repo=`echo $repo | sed "s/\/x86_64//g"`
@@ -34,9 +34,17 @@ download_url()
 	alias=`echo $alias | cut -d = -f 3 | sed "s/[\">]//g"`
 	#echo "repo alias is $alias"
 	if [ "$alias" = "" ]; then
-		echo "ERROR: alias is empty. exit"
-		echo "Current repo is $repo"
-		exit 1
+		echo "ERROR: alias is empty. try suse open build service style link."
+		repo=`echo $repo | sed "s/:\//:/g"`
+		#<repository type="rpm-md" alias="KIWI-NG_Appliance">
+		#<repository type="rpm-md" priority="1" alias="kiwi-common-boot">
+		alias=`grep $repo /home/vagrant/works/source/kiwi-descriptions/centos/x86_64/centos-07.0-JeOS/config.xml -B 1 | head -n1`
+		alias=`echo $alias | sed "s/^.*alias=\"\(.*\)\">[\ \t]*$/\1/g"`
+		if [ "$alias" = "" ]; then
+			echo "ERROR: alias is empty. exit"
+			echo "Current repo is $repo"
+			exit 1
+		fi
 	fi
 	REPO_PATH=/var/cache/kiwi/yum/cache/$alias
 	name=${url##*//}
@@ -44,14 +52,19 @@ download_url()
 	#echo package name is $name
 	echo $name | grep "\.rpm$"
 	if [ "$?" = "0" ]; then
-		echo "rpm package. download to $REPO_PATH/packages"
-		PACKAGE_FULL_NAME=$REPO_PATH/packages/$name
-		sudo rm $PACKAGE_FULL_NAME
-		$WGET --directory-prefix `dirname $PACKAGE_FULL_NAME` $url
+		echo -n "rpm package: "
+		PACKAGE_DIR_NAME=$REPO_PATH/packages
 	else
-		sudo rm /var/cache/kiwi/yum/cache/$alias/$name
-		$WGET --directory-prefix $REPO_PATH $url
+		echo -n "package metadata"
+		PACKAGE_DIR_NAME=/var/cache/kiwi/yum/cache/$alias/
 	fi
+	if ! [ -d "$PACKAGE_DIR_NAME" ]; then
+		echo "ERROR: package directory $PACKAGE_DIR_NAME does not exist. exit"
+		exit 1
+	fi
+	echo "download to $PACKAGE_DIR_NAME/$name"
+	sudo rm $PACKAGE_DIR_NAME/$name
+	$WGET --directory-prefix $PACKAGE_DIR_NAME $url
 }
 
 for url in `grep HTTP.Error.502 /home/vagrant/works/software/kiwi/build/image-root.log  | sed "s/^.*HTTP.Error.502\ :\ \(.*\)$/\1/g" | sort | uniq`; do
