1e94204f1941dce2e384ca607bf2b6ffdce140ff
Bamvor Zhang <bamv2005@gmail.com>
2018-01-26 10:44:04 +0800

    remove the functions which fail
    Signed-off-by: Bamvor Zhang <bamv2005@gmail.com>

--- productivity/Linux_environment/utils/functions.sh
+++ productivity/Linux_environment/utils/functions.sh
@@ -29,39 +29,3 @@ test_script(){
     fi
 }
 
-# smart means exit function in shell script but return from function call in
-# main shell. ONLY support bash at the monment.
-# Parameter:
-# $1: exit code. exit with 1 if missing.
-smart_exit(){
-    OLD_ENABLE_DEBUG=$ENABLE_DEBUG
-    ENABLE_DEBUG=false
-    ret=`test_script`
-    ENABLE_DEBUG=$OLD_ENABLE_DEBUG
-    exit_ret=$1
-    if [ "${exit_ret}" = "" ]; then
-        exit_ret=1
-    fi
-    if [ "$ret" = "true" ]; then
-        DEBUG "exit from script shell"
-        exit ${exit_ret}
-    else
-        DEBUG "return from function"
-        echo ${exit_ret}
-        return
-    fi
-}
-
-realpath_safe() {
-    path=`realpath --canonicalize-existing --quiet $1`
-    ret=$?
-    if [ "$ret" != "0" ]; then
-        echo "ERROR: invalid path $1. exit with $ret"
-        ENABLE_DEBUG=false
-        ret=`smart_exit $ret`
-        $?=$ret
-        return
-    fi
-    echo $path
-}
-
--- productivity/Linux_environment/utils/test__smart_exit.sh
+++
@@ -1,4 +0,0 @@
-
-source /Users/bamvor/works/open_log/productivity/Linux_environment/utils/functions.sh
-
-smart_exit $1
