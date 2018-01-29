31021bf446e07d69fb2032592d5ecccc3bd118a3
Bamvor Jian Zhang <bamv2005@gmail.com>
2018-01-29 11:58:40 +0800

    small fix for install_kiwi_remote
    1.  remote extra exit.
    2.  print arguments for debugging.
    
    Signed-off-by: Bamvor Jian Zhang <bamv2005@gmail.com>

--- eking/zhongruan_surveillance/install_kiwi_remote.sh
+++ eking/zhongruan_surveillance/install_kiwi_remote.sh
@@ -13,7 +13,6 @@ usage(){
 WORK_DIR=`realpath $0`
 WORK_DIR=`dirname ${WORK_DIR}`
 echo $WORK_DIR
-exit
 TARGET=$1
 shift 1
 
@@ -34,4 +33,5 @@ echo "Copy install_kiwi script to remote and run it with ssh default user with a
 #      all the parameter at the time of ssh of host system(there is a `shift 1`
 #      before the follow command). While the \$HOME will be expanded in the
 #      target system. reference <https://unix.stackexchange.com/questions/134114/how-do-i-pass-a-variable-from-my-local-server-to-a-remote-server/134116#134116>
+echo "arguments: $@"
 scp -p ${WORK_DIR}/install_kiwi.sh $TARGET:~/; ssh -t $TARGET "./install_kiwi.sh \$HOME $@"
