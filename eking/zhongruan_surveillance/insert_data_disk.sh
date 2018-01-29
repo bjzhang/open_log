c3fc20990aa37a6aaddc380cf863a3c913c10841
Bamvor Jian Zhang <bamv2005@gmail.com>
2018-01-29 18:18:09 +0800

    vm helper: insert disk: replace any name in source
    Signed-off-by: Bamvor Jian Zhang <bamv2005@gmail.com>

--- eking/zhongruan_surveillance/insert_data_disk.sh
+++ eking/zhongruan_surveillance/insert_data_disk.sh
@@ -26,7 +26,7 @@ if [ -f "/mnt/images/$DISK" ]; then
 fi
 sudo truncate -s 100g /mnt/images/$DISK
 sed "s/^\([\ \t]*<source file='\/mnt\/images\/\).*'\/>$/\1$DISK'\/>/g" -ibak $XML
-sed "s/^\([\ \t]*<target dev='\)vdX\(' bus='.*'\/>\)$/\1$VDISK\2/g" -ibak $XML
+sed "s/^\([\ \t]*<target dev='\).*\(' bus='.*'\/>\)$/\1$VDISK\2/g" -ibak $XML
 sudo virsh domblklist $NAME
 sudo virsh attach-device $NAME $XML --persistent --live
 sudo virsh domblklist $NAME
